211b2804cbb4ddafc0749c655ccfbc74c421988e
Jack Stein <jacklsteiniv@gmail.com>
2016-03-22 17:03:17 -0700

    Set up navigation/footer/header partials; main layout; linked stylesheet

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get about_me" do
+    get :about_me
+    assert_response :success
+  end
+
+end
