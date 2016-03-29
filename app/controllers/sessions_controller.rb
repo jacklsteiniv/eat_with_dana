28db38de87fa5581735c8eb15ba88fd886bfe1bd
Jack Stein <jacklsteiniv@gmail.com>
2016-03-29 15:22:18 -0700

    Login functional. Now, restrict posting to admins only.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,9 +9,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      remember user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user_path(current_user)
+      # remember user
+      # params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to root_path
       flash[:success] = "Welcome back to Vista, #{user.name}!"
     else
       flash[:danger] = 'Invalid login credentials - try again'
