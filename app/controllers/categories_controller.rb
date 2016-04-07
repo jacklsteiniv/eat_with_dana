3c9fdc85ef79afe4c5d67af7d8da675a59feb20c
Jack Stein <jacklsteiniv@gmail.com>
2016-04-07 11:06:52 -0700

    Added contact form gems. Updated category controller IDs to reflect accurate routing

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,17 +3,17 @@ class CategoriesController < ApplicationController
 
   #These 3 methods all use the index controller. Define routes
   def food
-    @cat = Category.find_by_id(4)
-    @posts = @cat.posts
-  end
-
-  def lifestyle
     @cat = Category.find_by_id(5)
     @posts = @cat.posts
   end
 
-  def recipes
+  def lifestyle
     @cat = Category.find_by_id(6)
+    @posts = @cat.posts
+  end
+
+  def recipes
+    @cat = Category.find_by_id(7)
     @posts = @cat.posts
   end
 
